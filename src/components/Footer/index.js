f3fd708f6cace2dcc446d154f98e0b00b7c774fe
Gonzalo Fernandez <gafernandez.web@gmail.com>
2020-10-04 19:17:43 -0300

    links to social network

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFacebook, faInstagram } from "@fortawesome/free-brands-svg-icons";
 import "./style.scss";
@@ -6,15 +7,18 @@ import "./style.scss";
 export default function Footer() {
   return (
     <div className="footerContainer">
-
       <div className="gradientSeparator"></div>
       <div className="socialContainer">
-        <div className="icons">
-          <FontAwesomeIcon size="2x" icon={faFacebook} />
-        </div>
-        <div className="icons">
-          <FontAwesomeIcon size="2x" icon={faInstagram} />
+        <a href="https://www.facebook.com/fede.nowback">
+          <div className="icons">
+            <FontAwesomeIcon size="2x" icon={faFacebook} />
           </div>
+        </a>
+        <a href="https://www.instagram.com/fedenowback">
+          <div className="icons">
+            <FontAwesomeIcon size="2x" icon={faInstagram} />
+          </div>
+        </a>
       </div>
       <span className="fedeNowBack">Fede Nowback</span>
     </div>
